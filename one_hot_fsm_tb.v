38285742aee2e16a9a1ae3cb0cc9f613232fcb62
AmitBastia <162047240+AmitBastia@users.noreply.github.com>
2025-07-29 03:13:19 +0530

    Update one_hot_fsm_tb.v

--- one_hot_fsm_tb.v
+++ one_hot_fsm_tb.v
@@ -2,8 +2,9 @@
 `timescale 1ns/1ps
 module one_hot_fsm_tb;
     reg in;
-    reg [9:0]state;
-    wire[9:0]next_state;
+    reg clk,rst;
+
+    wire [9:0]state,next_state;
     wire out1,out2;
     parameter s0 = 10'b0000000001,
               s1 = 10'b0000000010,
@@ -15,35 +16,39 @@ module one_hot_fsm_tb;
               s7 = 10'b0010000000,
               s8 = 10'b0100000000,
               s9 = 10'b1000000000;
-    top_module DUT(in,state,next_state,out1,out2);
+    top_module DUT(in,state,clk,rst,out1,out2);
+
+    always #5 clk = ~clk;
     
     initial 
         begin
             $dumpfile("fsm.vcd");
 
             $dumpvars;
-            $monitor($time, "state=%b, in=%b, next_state=%b, out1=%b, out2=%b", 
-                    state, in, next_state, out1, out2);
+            $monitor($time, "state=%b, in=%b,  out1=%b, out2=%b", 
+                    state, in,  out1, out2);
 
             in = 0;
-            state = s0;
-            #10;
+            clk = 0;
+            rst = 1; #10;
+            rst = 0; #10;
 
-            in = 1; state = s0; #10;
-            in = 1; state = s1; #10;
-            in = 1; state = s2; #10;
-            in = 1; state = s3; #10;
-            in = 1; state = s4; #10;
-            in = 1; state = s5; #10;
-            in = 1; state = s6; #10;
-            in = 1; state = s7; #10;
-            in = 1; state = s8; #10;
-            in = 1; state = s9; #10;
-            in = 0; state = s9; #10;
-            in = 0; state = s8; #10;
-            in = 0; state = s7; #10;
-            in = 0; state = s6; #10;
-            in = 0; state = s5; #10;
+            in = 1;  #10;
+            in = 1;  #10;
+            in = 1;  #10;
+            in = 1;  #10;
+            in = 1;  #10;
+            in = 1;  #10;
+            in = 1;  #10;
+            in = 1;  #10;
+            in = 1;  #10;
+            in = 1;  #10;
+            in = 1;  #10;
+            in = 0;  #10;
+            in = 0;  #10;
+            in = 0;  #10;
+            in = 0;  #10;
+            in = 0;  #10;
 
             $finish;
         end
